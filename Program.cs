33cb6e021f9db96e001615fc7a9747a140b982df
Евгений Кучма <desferto87@gmail.com>
2024-06-24 22:04:27 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkSixteenPowerNumberTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int minNumber = 4;
+            int maxNumber = 100;
+            int numberPower = 2;
+            int numberPowerTwo = 2;
+            int countPowerNumber = 0;
+
+            Random random = new Random();
+            number = random.Next(minNumber, maxNumber);
+
+            while(numberPower < number)
+            {
+                numberPower *= numberPowerTwo;
+                ++countPowerNumber;
+            }
+
+            Console.WriteLine($"the minimum power of two greater than the given {number} " +
+                $"is equal to the degree of the number {countPowerNumber}");
+        }
+    }
+}
